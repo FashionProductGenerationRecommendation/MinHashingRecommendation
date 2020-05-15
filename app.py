7028ea254501eb9e3977fda45c81b6f164a15773
KeshavAdityaRP <keshavaditya26896@gmail.com>
2020-05-14 21:30:37 -0400

    Products : 60k + Number of Hashes Function : 500

--- app.py
+++ app.py
@@ -14,33 +14,35 @@ def formatRow(sentence):
     productDiscription = list(set(productDiscription))
     return (productId, productDiscription) 
 
-def hash_(word, productId, b, numberOfHashFunctions):
+def hash_(word, productId, a, b, numberOfHashFunctions):
     uniqueWordNumber = int(hashlib.sha256(word.encode('utf-8')).hexdigest(), 16)
     output = []
     for i in range(0,numberOfHashFunctions):
-        hashName = "hash-" + str(i+1)
-        hashValue = ((uniqueWordNumber) + b[i]) % (10**5)
+        hashName = i + 1
+        hashValue = ((uniqueWordNumber) + b[i]) % ((10**5) + a[i])
         output.append(((productId, hashName),hashValue))
     return output     
 
-numberOfHashFunctions = 3
+numberOfHashFunctions = 500
 b = [(2*i) + 7 for i in range(0, numberOfHashFunctions)]
-columnNames = ["productId"] + ["hash-" + str(i+1) for i in range(0, numberOfHashFunctions)]
+a = [i + 3 for i in range(0, numberOfHashFunctions)]
 sc = SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
 numberOfHashFunctions = sc.broadcast(numberOfHashFunctions)
-columnNames = sc.broadcast(columnNames)
+a = sc.broadcast(a)
 b = sc.broadcast(b)
-productsDataset = sc.textFile('/home/keshav/Stony Brook/Big Data/MinHashingRecommendation/testStyles.csv')
+productsDataset = sc.textFile('/home/keshav/Stony Brook/Big Data/MinHashingRecommendation/styles.csv')
 productsAndItsDiscription = productsDataset.map(lambda row: formatRow(row))
 wordAndProductAssociation = productsAndItsDiscription.flatMap(lambda value: [(x, value[0]) for x in value[1]])
-productIdHashNameAndHashValue = wordAndProductAssociation.flatMap(lambda value: hash_(value[0],value[1], b.value, numberOfHashFunctions.value))
+productIdHashNameAndHashValue = wordAndProductAssociation.flatMap(lambda value: hash_(value[0],value[1], a.value, b.value, numberOfHashFunctions.value))
 signatureMatrix = productIdHashNameAndHashValue.reduceByKey(lambda a,b: min(a,b))
 productIdAndHashNameMinHashValue = signatureMatrix.map(lambda x: (x[0][0],(x[0][1],x[1])))
-productIdAndHashNameMinHashValueList = productIdAndHashNameMinHashValue.groupByKey().map(lambda x: (x[0], str(list(x[1]))))
-df = sqlContext.createDataFrame(productIdAndHashNameMinHashValueList,  ['productId', 'hashInformation'])
-df.toPandas().to_csv("/home/keshav/Stony Brook/Big Data/MinHashingRecommendation/signatureMatrix.csv", header=True)
+productIdAndHashNameMinHashValueList = productIdAndHashNameMinHashValue.groupByKey().map(lambda x: (x[0], list(x[1])))
+productIdAndHashNameMinHashValueSorted = productIdAndHashNameMinHashValueList.map(lambda x: (x[0], str(sorted(x[1], key=lambda val: val[0]))))
+df = sqlContext.createDataFrame(productIdAndHashNameMinHashValueSorted)
+df.toPandas().to_csv("/home/keshav/Stony Brook/Big Data/MinHashingRecommendation/signatureMatrix.csv", header=False)
 
+# productIdAndHashNameMinHashValue = signatureMatrix.map(lambda x: (x[0][0],(x[0][1],x[1])))
 # a = [i+1 for i in range(0, numberOfHashFunctions)]
 # print (hashName, " = ", uniqueWordNumber, "+", b)
 # createRow = signatureMatrix.map(lambda x: (x[0][0], x[0][1], x[1]))
--- findSimilarProducts.py
+++ findSimilarProducts.py
@@ -1,8 +1,52 @@
-import ast
-import numpy as np
-import pandas as pd
-
-# res = ast.literal_eval(ini_list) 
+# Find ProductIds Similar to ProductId : 9204
 # 9204,Men,Footwear,Shoes,Casual Shoes,Black,Summer,2011,Casual,Puma Men Future Cat Remix SF Black Casual Shoes
 # 53759,Men,Apparel,Topwear,Tshirts,Grey,Summer,2012,Casual,Puma Men Grey T-shirt
 # 29114,Men,Accessories,Socks,Socks,Navy Blue,Summer,2012,Casual,Puma Men Pack of 3 Socks
+# 1855,Men,Apparel,Topwear,Tshirts,Grey,Summer,2011,Casual,Inkfruit Mens Chain Reaction T-shirt
+# 33846,Men,Footwear,Shoes,Casual Shoes,Black,Summer,2012,Casual,Puma Men Future Cat Black Shoes
+
+import re
+import ast
+import hashlib
+import numpy as np
+from pyspark import SparkContext, SparkConf
+
+def convertToList(s):
+    return eval( "[%s]" % s )
+
+def processLine(line):
+    start = line.find(',')
+    line = line[start+1:]
+    end = line.find(',')
+    productId = int(line[0: end])
+    line = line[end+1:]
+    correspondingHashInformation = ast.literal_eval(line)
+    correspondingHashInformation = convertToList(correspondingHashInformation)[0]
+    return (productId, correspondingHashInformation)
+
+def findSimilarProducts(baseProduct, product):
+    numberOfHashes = len(baseProduct[1])
+    intersection = 0
+    for i in range(0, numberOfHashes):
+        if baseProduct[1][i][1] == product[1][i][1]:
+            intersection += 1
+    similarityScore = intersection / numberOfHashes
+    return ((baseProduct[0], product[0]), similarityScore)
+
+sc = SparkContext.getOrCreate()
+path = '/home/keshav/Stony Brook/Big Data/MinHashingRecommendation/signatureMatrix.csv'
+baseProductId = 9204
+baseProductId = sc.broadcast(baseProductId)
+signatureMatrixDataset = sc.textFile(path)
+signatureMatrixDatasetSplit = signatureMatrixDataset.map(lambda line: processLine(line))
+baseProduct = signatureMatrixDatasetSplit.filter(lambda x: x[0] == baseProductId.value).collect()[0]
+baseProduct = sc.broadcast(baseProduct)
+similarityScoreForEachProductWrtBaseProduct = signatureMatrixDatasetSplit.map(lambda product: findSimilarProducts(baseProduct.value, product))
+similarityScoreForEachProductWrtBaseProductFiltered = similarityScoreForEachProductWrtBaseProduct.filter(lambda x: x[0][0] != x[0][1]) 
+sortedBySimilarityScore = similarityScoreForEachProductWrtBaseProductFiltered.top(20, key=lambda x: x[1])
+print (sortedBySimilarityScore)
+
+# Results
+# Base Product ID : 9204
+# Top 20 Recommendations for ProductId : 9204
+# [((9204, 33846), 0.696), ((9204, 33843), 0.696), ((9204, 26635), 0.696), ((9204, 6380), 0.69), ((9204, 22912), 0.674), ((9204, 13989), 0.664), ((9204, 26637), 0.642), ((9204, 18473), 0.638), ((9204, 8914), 0.622), ((9204, 9137), 0.59), ((9204, 5896), 0.576), ((9204, 22162), 0.576), ((9204, 22199), 0.576), ((9204, 26639), 0.572), ((9204, 38534), 0.566), ((9204, 12892), 0.564), ((9204, 26636), 0.564), ((9204, 33845), 0.564), ((9204, 38533), 0.564), ((9204, 33847), 0.564)]
